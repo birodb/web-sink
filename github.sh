06c7152990f41936031bd4fd132af9625f365977
botond <botond23-git@google.com>
2016-02-02 21:10:20 +0100

    first commit

---
+++ github.sh
@@ -0,0 +1,6 @@
+echo "# web-sink" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/birodb/web-sink.git
+git push -u origin master
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject restful-clojure "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.xerial/sqlite-jdbc "3.8.10.1"]
+                 [enlive "1.1.6"]
+                 [hiccup "1.0.5"]])
+  ;:main restful-clojure.core/mymain)
